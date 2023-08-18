6f6c4efe2641c35e3d89943167ecc9fcee11a822
andreajiao <andreajiao@gmail.com>
2023-08-18 13:55:51 +0200

    gitignore 5

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL:"https://api.rawg.io/api",
     params:{
-        key: "teste"
+         key: "3d3ef583fd3f4cb4b938fa047b48cb6d"
     }
 })
